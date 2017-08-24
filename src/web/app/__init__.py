310d7266ab52434c52a2f760bebe887aac9e1d1c
LUCCAT\Lucca <lbt86@msstate.edu>
2017-08-24 15:18:09 -0500

    added JSON file creation

--- src/web/app/__init__.py
+++ src/web/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
@@ -8,7 +9,18 @@ def index():
     pedal = [0, 0, 0, 0, 0, 0, 0]
     pedal_in = [0, 0, 0, 0, 0, 0, 0]
     pedal_out = [0, 0, 0, 0, 0, 0, 0]
-    
+
+    bank_name = request.json['bank_name']
+    bank_num = request.json['bank_num']
+    preset_num = request.json['preset_num']
+    preset_name = request.json['preset_name']
+    pedal_pos1_name = request.json['pedal_pos1_name']
+    pedal_pos2_name = request.json['pedal_pos2_name']
+    pedal_pos3_name = request.json['pedal_pos3_name']
+    pedal_pos4_name = request.json['pedal_pos4_name']
+    pedal_pos5_name = request.json['pedal_pos5_name']
+    pedal_pos6_name = request.json['pedal_pos6_name']
+    pedal_pos7_name = request.json['pedal_pos7_name']
     pedal[0] = request.json['pedal_pos1']
     pedal[1] = request.json['pedal_pos2']
     pedal[2] = request.json['pedal_pos3']
@@ -16,7 +28,7 @@ def index():
     pedal[4] = request.json['pedal_pos5']
     pedal[5] = request.json['pedal_pos6']
     pedal[6] = request.json['pedal_pos7']
-    #print(pedal)
+
     # 0 indicates unused pedal, 8 indicates final output
     pedal_in[0] = pedal[0]
     pedal_out[0] = pedal[1]
@@ -41,24 +53,44 @@ def index():
             pedal_out[(i - 1)] = 8
             break
 
-    #print(pedal_out)
-    #return "Dr Dabs is gonna love this shit"
-    return jsonify({
-    'pedal_pos1_in': pedal_in[0],
-    'pedal_pos1_out': pedal_out[0],
-    'pedal_pos2_in': pedal_in[1],
-    'pedal_pos2_out': pedal_out[1],
-    'pedal_pos3_in': pedal_in[2],
-    'pedal_pos3_out': pedal_out[2],
-    'pedal_pos4_in': pedal_in[3],
-    'pedal_pos4_out': pedal_out[3],
-    'pedal_pos5_in': pedal_in[4],
-    'pedal_pos5_out': pedal_out[4],
-    'pedal_pos6_in': pedal_in[5],
-    'pedal_pos6_out': pedal_out[5],
-    'pedal_pos7_in': pedal_in[6],
-    'pedal_pos7_out': pedal_out[6],
-    })
+    data = {
+        'bank_name': bank_name,
+        'bank_num': bank_num,
+        'preset_name': preset_name,
+        'preset_num': preset_num,
+        "pedal_pos1_name": pedal_pos1_name,
+        "pedal_pos2_name": pedal_pos2_name,
+        "pedal_pos3_name": pedal_pos3_name,
+        "pedal_pos4_name": pedal_pos4_name,
+        "pedal_pos5_name": pedal_pos5_name,
+        "pedal_pos6_name": pedal_pos6_name,
+        "pedal_pos7_name": pedal_pos7_name,
+        'pedal_pos1_in': pedal_in[0],
+        'pedal_pos1_out': pedal_out[0],
+        'pedal_pos2_in': pedal_in[1],
+        'pedal_pos2_out': pedal_out[1],
+        'pedal_pos3_in': pedal_in[2],
+        'pedal_pos3_out': pedal_out[2],
+        'pedal_pos4_in': pedal_in[3],
+        'pedal_pos4_out': pedal_out[3],
+        'pedal_pos5_in': pedal_in[4],
+        'pedal_pos5_out': pedal_out[4],
+        'pedal_pos6_in': pedal_in[5],
+        'pedal_pos6_out': pedal_out[5],
+        'pedal_pos7_in': pedal_in[6],
+        'pedal_pos7_out': pedal_out[6]
+    }
+
+    # Write to JSON
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+    # Read JSON
+    """with open('data.json', 'r') as f:
+        data = json.load(f)"""
+
+    return "Success"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5052, debug=True)
