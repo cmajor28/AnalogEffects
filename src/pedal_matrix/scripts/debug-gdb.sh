0d4a66e9278162edc6156df93b494ba79f961586
LUCCAT\Lucca <lbt86@msstate.edu>
2017-11-16 15:03:08 -0600

    Merge branch 'dev' of https://github.com/cmajor28/AnalogEffects into dev
    # Conflicts:
    #	src/pedal_matrix/drivers/gpio.c
    #	src/pedal_matrix/drivers/mt8809.c
    #	src/pedal_matrix/inc/pins.h
    #	src/pedal_matrix/inc/utils.h
    #	src/pedal_matrix/main.c

---
+++ src/pedal_matrix/scripts/debug-gdb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo hciconfig hci0 down
+sudo hciconfig hci0 up
+export DISPLAY=:0
+xhost local:root
+cd ../
+sudo DISPLAY=$DISPLAY gdb python3
---
+++ src/pedal_matrix/scripts/pedal_matrix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+log_dir=/home/debian/pedal_matrix/logs
+if [ ! -d "$log_dir" ]; then
+    mkdir &log_dir
+fi
+log_file=$log_dir/log.txt
+echo "" > $log_file
+echo "Starting Pedal Matrix..." >> $log_file
+echo "------------------------" >> $log_file
+
+sudo /home/debian/pedal_matrix/scripts/fix_gpio.sh &>> $log_file
+
+sudo hciconfig hci0 down &>> $log_file
+sudo hciconfig hci0 up &>> $log_file
+cd /home/debian/pedal_matrix
+if [ ! -d "data" ]; then
+    mkdir data
+fi
+sudo python3 -u __init__.py &>> $log_file
+echo "Exiting Pedal Matrix..." >> $log_file
+echo "------------------------" >> $log_file
