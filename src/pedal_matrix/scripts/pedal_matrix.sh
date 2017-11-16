d1b9b2832bccee30b6117353357c5774405b0ef9
Caleb Merchant <cmajor.merchant@gmail.com>
2017-11-16 16:19:30 -0600

    Fixed shell script

--- src/pedal_matrix/scripts/pedal_matrix.sh
+++ src/pedal_matrix/scripts/pedal_matrix.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 log_dir=/home/debian/pedal_matrix/logs
-if [ ! -d "$log_dir" ]; then
-    mkdir &log_dir
-fi
+if [ ! -d "$log_dir" ]; then mkdir $log_dir; fi
 log_file=$log_dir/log.txt
 echo "" > $log_file
 echo "Starting Pedal Matrix..." >> $log_file
@@ -14,9 +12,7 @@ sudo /home/debian/pedal_matrix/scripts/fix_gpio.sh &>> $log_file
 sudo hciconfig hci0 down &>> $log_file
 sudo hciconfig hci0 up &>> $log_file
 cd /home/debian/pedal_matrix
-if [ ! -d "data" ]; then
-    mkdir data
-fi
+if [ ! -d "data" ]; then mkdir data; fi
 sudo python3 -u __init__.py &>> $log_file
 echo "Exiting Pedal Matrix..." >> $log_file
 echo "------------------------" >> $log_file
